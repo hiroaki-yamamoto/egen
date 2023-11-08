ee63cba1c023c4c570df5b045845d2e647b5ea32
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-09 07:51:16 +0900

    feat(entities): :sparkles: Add type to field

--- src/entities/inputs/field.rs
+++ src/entities/inputs/field.rs
@@ -1,8 +1,13 @@
-use super::rs::Rust;
 use ::serde::Deserialize;
 
+use super::rs::Rust;
+use super::PrimitiveTypes;
+
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Field {
   pub name: String,
+  #[serde(rename = "type")]
+  pub f_type: PrimitiveTypes,
   pub rust: Option<Rust>,
 }
