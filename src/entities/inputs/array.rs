75ce0151cdd0edc5184ed29233dda288613954c6
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-09 16:06:34 +0900

    chore(entities): :fire: Removed duplicated rust option

--- src/entities/inputs/array.rs
+++ src/entities/inputs/array.rs
@@ -1,12 +1,10 @@
 use ::serde::Deserialize;
 
 use super::primitives::PrimitiveTypes;
-use super::rs::Rust;
 
 #[derive(Debug, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct ArrayProperty {
-  pub rust: Option<Rust>,
   pub item: Box<PrimitiveTypes>,
 }
 
@@ -14,7 +12,6 @@ impl ArrayProperty {
   #[cfg(test)]
   pub fn new(item_type: PrimitiveTypes) -> Self {
     return Self {
-      rust: None,
       item: Box::new(item_type),
     };
   }
