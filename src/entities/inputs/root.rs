2e6432bb00b2e917c3ee546e43b5298db8f15937
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-09 21:28:05 +0900

    feat(entities): :sparkles: Add type field

--- src/entities/inputs/root.rs
+++ src/entities/inputs/root.rs
@@ -4,22 +4,22 @@ use super::enumeration::Enumeration;
 use super::structure::Structure;
 
 #[derive(Debug, Deserialize, PartialEq, Eq)]
-#[serde(rename_all = "camelCase", untagged)]
+#[serde(rename_all = "camelCase", tag = "type")]
 pub enum Root {
-  Enumeration(Enumeration),
-  Structure(Structure),
+  Enum(Enumeration),
+  Struct(Structure),
 }
 
 #[cfg(test)]
 impl From<Structure> for Root {
   fn from(structure: Structure) -> Self {
-    return Root::Structure(structure);
+    return Root::Struct(structure);
   }
 }
 
 #[cfg(test)]
 impl From<Enumeration> for Root {
   fn from(enumeration: Enumeration) -> Self {
-    return Root::Enumeration(enumeration);
+    return Root::Enum(enumeration);
   }
 }
