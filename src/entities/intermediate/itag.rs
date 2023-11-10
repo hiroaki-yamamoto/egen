098f3b7d76e91f7ee9169072602f6f1b5fb2db14
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-10 13:24:12 +0900

    feat(entities): :sparkles: Add rs_module_name function

--- src/entities/intermediate/itag.rs
+++ src/entities/intermediate/itag.rs
@@ -3,6 +3,7 @@ use ::std::sync::Arc;
 pub trait ITag {
   /// Return the hint of class name of the structure.
   fn class_name(&self) -> Arc<String>;
-  // fn rs_module_name(&self) -> &str;
+  /// Return the hint of module name in Rust.
+  fn rs_module_name(&self) -> Arc<String>;
   // fn ts_module_name(&self) -> &str;
 }
--- src/entities/intermediate/tag.rs
+++ src/entities/intermediate/tag.rs
@@ -1,5 +1,8 @@
-use crate::entities::inputs::Root;
 use ::std::sync::Arc;
+
+use ::regex::{Error as RegexError, Regex};
+
+use crate::entities::inputs::Root;
 
 use super::itag::ITag;
 
@@ -7,27 +10,38 @@ use super::itag::ITag;
 pub struct Tag {
   raw_name: String,
   root: Root,
+  re: Regex,
 }
 
 impl Tag {
-  pub fn new(name: String, root: Root) -> Self {
-    return Self {
+  pub fn new(name: String, root: Root) -> Result<Self, RegexError> {
+    return Ok(Self {
       raw_name: name,
       root,
-    };
+      re: Regex::new(r"[[:^alnum:]]")?,
+    });
   }
 }
 
 impl ITag for Tag {
   fn class_name(&self) -> Arc<String> {
-    let mut name = self.raw_name.to_string();
-    name = name.replace("-", "_");
+    let mut name = self.raw_name.to_string().replace("-", "_");
     let split_capitalized_names = name.split("_").map(|name| {
-      let mut name = name.to_string();
+      let mut name = self.re.replace_all(name, "").to_string();
       name = name.remove(0).to_uppercase().to_string() + &name;
       return name;
     });
     name = split_capitalized_names.collect::<Vec<String>>().join("");
+    return Arc::new(name);
+  }
+
+  fn rs_module_name(&self) -> Arc<String> {
+    let mut name = self.raw_name.to_string().replace("-", "_");
+    let split_capitalized_names = name.split("_").map(|name| {
+      let name = self.re.replace_all(&name, "").to_lowercase();
+      return name;
+    });
+    name = split_capitalized_names.collect::<Vec<String>>().join("_");
     return Arc::new(name);
   }
 }
@@ -40,12 +54,24 @@ mod test {
   use super::Tag;
 
   #[test]
-  pub fn test_class_name() {
+  fn test_class_name() {
     let tag = Tag::new(
-      "class_name-test".to_string(),
+      "@ cla\tss_na||☺me-te\nst|\\]';".to_string(),
       Root::Struct(Structure::new()),
-    );
+    )
+    .unwrap();
     let name = tag.class_name();
     assert!(name.as_ref() == "ClassNameTest", "name: {:?}", name);
+  }
+
+  #[test]
+  fn test_rs_module_name() {
+    let tag = Tag::new(
+      "@ cla\tss_na||☺me-te\nst|\\]';".to_string(),
+      Root::Struct(Structure::new()),
+    )
+    .unwrap();
+    let name = tag.rs_module_name();
+    assert!(name.as_ref() == "class_name_test", "name: {:?}", name);
   }
 }
