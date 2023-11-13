b0678d054f41d280a5f2ae895cf705989c22fbcb
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-13 14:35:41 +0900

    feat(service): :sparkles: Add required imports

--- src/services/output/rust.rs
+++ src/services/output/rust.rs
@@ -1,20 +1,47 @@
+use ::std::io::Write;
+use ::std::marker::PhantomData;
+use ::std::sync::Arc;
+
 use ::minijinja::{Environment, Template};
 
+use crate::entities::intermediate::ITag;
+
+use super::interface::IOutput;
 use super::OutputResult;
 
-pub struct Rust<'env> {
+pub struct Rust<'env, Writer>
+where
+  Writer: Write,
+{
   env: Environment<'env>,
+  _w: PhantomData<Writer>,
 }
 
-impl<'env> Rust<'env> {
-  pub fn new() -> OutputResult<Self> {
+impl<'env, Writer> Rust<'env, Writer>
+where
+  Writer: Write,
+{
+  /// Initialize a new instance.
+  ///
+  /// ### Parameters:
+  /// * `modules` - The list of modules to be imported. Note that this should be
+  ///  the list of modules that is proceeded by ImportExtractor.
+  pub fn new(modules: Vec<Arc<dyn ITag>>) -> OutputResult<Self> {
+    let modules: Vec<&str> = modules
+      .iter()
+      .map(|tag| tag.rs_module_name().as_str())
+      .collect();
     let mut env: Environment<'env> = Environment::new();
+    env.add_global("tags", modules);
     env.add_template(
       "struct",
       include_str!("../../templates/struct.rs.jinja"),
     )?;
     env.add_template("enum", include_str!("../../templates/enum.rs.jinja"))?;
-    return Ok(Self { env });
+    return Ok(Self {
+      env,
+      _w: PhantomData,
+    });
   }
 
   fn struct_template(&self) -> OutputResult<Template> {
@@ -24,4 +51,13 @@ impl<'env> Rust<'env> {
   fn enum_template(&self) -> OutputResult<Template> {
     return Ok(self.env.get_template("enum")?);
   }
+}
+
+impl<'env, Writer> IOutput for Rust<'env, Writer>
+where
+  Writer: Write,
+{
+  type Writer = Writer;
+
+  fn render(&self, writer: &mut Self::Writer) -> OutputResult<()> {}
 }
