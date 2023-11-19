4267507039367fb2773e5eef239a17b821166cce
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-20 07:10:54 +0900

    test(template): :white_check_mark: Pass the test

--- src/fixtures/zod_ts_out/complex.zod.ts
+++ src/fixtures/zod_ts_out/complex.zod.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod';
 
-import { SimpleStructure } from './simple-structure.zod.ts';
 import { Reference } from './reference.zod.ts';
+import { SimpleStructure } from './simple-structure.zod.ts';
 
 export const Complex = z.object({
   code: z.number().max(65535).min(0),
-  simpleText: z.string(),
-  simpleArray: z.array(z.string()),
+  detailedText: z.string(),
   referenceArray: z.array(z.lazy(() => SimpleStructure)),
-  selfReferenceArray: z.array(z.lazy(() => Complex)),
   secondReference: z.lazy(() => Reference),
+  selfReferenceArray: z.array(z.lazy(() => Complex)),
+  simpleArray: z.array(z.string()),
+  simpleText: z.string(),
 });
