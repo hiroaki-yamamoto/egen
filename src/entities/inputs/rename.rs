106f58a4c86e558b2b205ebab6a9b85a1258fee2
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-12-10 10:40:28 +0900

    feat(entities): :sparkles: Add pascal and kebab case for availabe rename option

--- src/entities/inputs/rename.rs
+++ src/entities/inputs/rename.rs
@@ -4,10 +4,14 @@ use ::serde::{Deserialize, Serialize};
 #[cfg_attr(test, derive(Clone))]
 #[serde(rename_all = "camelCase")]
 pub enum Rename {
+  /// PascalCase
+  PascalCase,
   /// camelCase
   CamelCase,
   /// snake_case
   SnakeCase,
+  /// kebab-case
+  KebabCase,
 }
 
 impl Default for Rename {
